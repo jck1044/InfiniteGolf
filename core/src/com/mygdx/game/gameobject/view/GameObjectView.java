91bbb161bb55028a76e9f6f68c46a51c6790b382
jck1044 <jck1044@wildcats.unh.edu>
2024-02-07 13:37:11 -0500

    sky background shows

---
+++ core/src/com/mygdx/game/gameobject/view/GameObjectView.java
@@ -0,0 +1,17 @@
+package com.mygdx.game.gameobject.view;
+
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+
+public abstract class GameObjectView {
+    protected SpriteBatch batch;
+
+    public GameObjectView(SpriteBatch batch) {
+        this.batch = batch;
+    }
+
+    public abstract void render(float dt);
+
+    public short getLayer() {
+        return Short.MAX_VALUE;
+    }
+}
