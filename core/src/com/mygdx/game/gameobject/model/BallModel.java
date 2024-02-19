5ab50a9bb35ed2e66bdfad8f4b1ca0cda888abfc
jck1044 <jck1044@wildcats.unh.edu>
2024-02-19 13:30:01 -0500

    Simplified ball sprite - should be less resource intensive. Made ball spin slower and in the direction that it is moving.

--- core/src/com/mygdx/game/gameobject/model/BallModel.java
+++ core/src/com/mygdx/game/gameobject/model/BallModel.java
@@ -5,10 +5,12 @@ import com.badlogic.gdx.math.Vector2;
 public class BallModel {
     private Vector2 position; // vector for x, y location of the ship in the game world
     private float speed = 200F;
+    private final float radius;
     private Vector2 movementVector = new Vector2(0, 0);
 
-    public BallModel(Vector2 position) {
+    public BallModel(Vector2 position, float radius) {
         this.position = position;
+        this.radius = radius;
     }
 
     public Vector2 getPosition() {
@@ -18,6 +20,10 @@ public class BallModel {
     public void updatePosition(float dt) {
         Vector2 posChange = this.movementVector.cpy().scl(speed * dt);
         this.position.add(posChange);
+    }
+
+    public float getRadius() {
+        return radius;
     }
 
     public Vector2 getMovementVector() {
--- core/src/com/mygdx/game/gameobject/view/BallView.java
+++ core/src/com/mygdx/game/gameobject/view/BallView.java
@@ -17,44 +17,30 @@ public class BallView {
     protected int currentAnimationSequence = -1;
     protected TextureRegion currentAnimationFrame;
     private float stateTime;
-
     final float animationFrameDuration = 0.5F;
+    private int rotationAngle;
+    final int rotationDegrees = 50;
 
     public BallView(BallModel model, SpriteBatch batch) {
         this.model = model;
         this.batch = batch;
         this.textureAtlas = new TextureAtlas("textures/ball.txt");
         this.animationFrames = new Array<>();
-        animationFrames.add(textureAtlas.createSprites("ball_0"));
-        animationFrames.add(textureAtlas.createSprites("ball_1"));
-        animationFrames.add(textureAtlas.createSprites("ball_2"));
-        animationFrames.add(textureAtlas.createSprites("ball_3"));
+        animationFrames.add(textureAtlas.createSprites("ball"));
         currentAnimation = new Animation<>(this.animationFrameDuration, animationFrames.get(0));
         stateTime = 0f;
+        this.rotationAngle = 0;
     }
 
     public void render(float dt) {
-        if (this.model.getMovementVector().x != 0 || this.model.getMovementVector().y != 0) { //ball is moving --> should be rotating
-            int nextAnimationSequence = getNextBallRotation(currentAnimationSequence);
-            Array<Sprite> spriteList = animationFrames.get(nextAnimationSequence);
-            currentAnimation = new Animation<>(this.animationFrameDuration, spriteList);
-            currentAnimationSequence = nextAnimationSequence;
+        if (this.model.getMovementVector().x > 0) { //moving right
+            this.rotationAngle = (this.rotationAngle + rotationDegrees) % 360;
+        } else if (this.model.getMovementVector().x < 0) { //moving left
+            this.rotationAngle = (this.rotationAngle - rotationDegrees + 360) % 360;
         }
         stateTime += dt;
         currentAnimationFrame = (TextureRegion) currentAnimation.getKeyFrame(stateTime, true);
-        batch.draw(currentAnimationFrame, this.model.getPosition().x, this.model.getPosition().y, 128, 128);
-    }
-
-    public int getNextBallRotation(int currentAnimationSequence) {
-        if (currentAnimationSequence == 0) {
-            return 1;
-        } else if (currentAnimationSequence == 1) {
-            return 2;
-        } else if (currentAnimationSequence == 2) {
-            return 3;
-        } else {
-            return 0;
-        }
+        batch.draw(currentAnimationFrame, this.model.getPosition().x, this.model.getPosition().y, this.model.getRadius(), this.model.getRadius(), this.model.getRadius() * 2, this.model.getRadius() * 2, 1, 1, this.rotationAngle);
     }
 
     public void dispose() {
