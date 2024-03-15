4357566bbca576af7358a38162b5f662b70bbb51
jck1044 <jck1044@wildcats.unh.edu>
2024-03-15 14:37:07 -0700

    expanding power ball and arrow on screen

--- core/src/com/infinitegolf/game/Application.java
+++ core/src/com/infinitegolf/game/Application.java
@@ -12,9 +12,7 @@ import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.maps.MapProperties;
 import com.badlogic.gdx.maps.tiled.TiledMap;
 import com.badlogic.gdx.maps.tiled.TmxMapLoader;
-import com.badlogic.gdx.maps.tiled.renderers.BatchTiledMapRenderer;
 import com.badlogic.gdx.maps.tiled.renderers.OrthogonalTiledMapRenderer;
-import com.badlogic.gdx.math.Rectangle;
 import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.math.Vector3;
 import com.badlogic.gdx.physics.box2d.Body;
@@ -28,16 +26,25 @@ import com.badlogic.gdx.utils.ScreenUtils;
 import Utils.TileObjectUtil;
 
 public class Application extends ApplicationAdapter {
+    private final float golfBallSize = 16;
     private boolean DEBUG = false;
     private final float SCALE = 2.5f;
     SpriteBatch batch;
     Texture ballTexture;
+    Texture arrowTexture;
     private OrthographicCamera camera;
     private OrthogonalTiledMapRenderer tmr;
     private TiledMap map;
     private Box2DDebugRenderer b2dr;
     private World world;
     private Body golfBall, platform;
+    private float horizontalForce = 0;
+    private float verticalForce = 0;
+    //    private float power = 0;
+    private Boolean powerUp = true;
+    private float powerBallSize = golfBallSize;
+    private final float arrowSize = 32;
+    private float ballAngle;
 
     @Override
     public void create() {
@@ -54,6 +61,7 @@ public class Application extends ApplicationAdapter {
 
         batch = new SpriteBatch();
         ballTexture = new Texture("Images/GolfBall.png");
+        arrowTexture = new Texture("Images/Arrow.png");
 
         map = new TmxMapLoader().load("Maps/Hole1.tmx");
         tmr = new OrthogonalTiledMapRenderer(map); // Can pass in batch??? for shaders???
@@ -73,12 +81,32 @@ public class Application extends ApplicationAdapter {
 
         batch.begin();
         Sprite ballSprite = new Sprite(ballTexture);
-        float angle = golfBall.getAngle() * (180 / (float) Math.PI);
-        ballSprite.rotate(angle);
-        ballSprite.setX(golfBall.getPosition().x * PPM - (ballTexture.getWidth() / 2));
-        ballSprite.setY(golfBall.getPosition().y * PPM - (ballTexture.getHeight() / 2));
+        ballAngle = golfBall.getAngle() * (180 / (float) Math.PI);
+        ballSprite.rotate(ballAngle);
+        ballSprite.setX(golfBall.getPosition().x * PPM - ((float) ballTexture.getWidth() / 2));
+        ballSprite.setY(golfBall.getPosition().y * PPM - ((float) ballTexture.getHeight() / 2));
+
+        Sprite powerSprite = new Sprite(ballTexture);
+//        powerSprite.setOrigin(ballSprite.getOriginX(), ballSprite.getOriginY());
+        powerSprite.setX(golfBall.getPosition().x * PPM - ((ballTexture.getWidth() * (powerBallSize / golfBallSize)) / 2));
+        powerSprite.setY(golfBall.getPosition().y * PPM - ((ballTexture.getHeight() * (powerBallSize / golfBallSize)) / 2));
+        powerSprite.setSize(powerBallSize, powerBallSize);
+        powerSprite.draw(batch);
+
+
+        Sprite arrowSprite = new Sprite(arrowTexture);
+        arrowSprite.setX(golfBall.getPosition().x * PPM - ((ballTexture.getWidth() * (arrowSize / golfBallSize)) / 2));
+        arrowSprite.setY(golfBall.getPosition().y * PPM - ((ballTexture.getHeight() * (arrowSize / golfBallSize)) / 2));
+        arrowSprite.setSize(arrowSize, arrowSize);
+//        arrowSprite.setOrigin(((golfBall.getPosition().x * PPM) - ((float) ballTexture.getWidth() / 2)), (golfBall.getPosition().y * PPM));
+//        arrowSprite.setOrigin(golfBall.getPosition().x + ((float) ballTexture.getWidth() / 1), golfBall.getPosition().y + ((float) ballTexture.getHeight() / 1));
+//        arrowSprite.rotate90(false);
+//        arrowSprite.rotate90(false);
+        arrowSprite.draw(batch);
+
+
         ballSprite.draw(batch);
-//		batch.draw(ballTexture,golfBall.getPosition().x * PPM - (ballTexture.getWidth()/2),golfBall.getPosition().y * PPM - (ballTexture.getHeight()/2));
+        //		batch.draw(ballTexture,golfBall.getPosition().x * PPM - (ballTexture.getWidth()/2),golfBall.getPosition().y * PPM - (ballTexture.getHeight()/2));
         batch.end();
 
         b2dr.render(world, camera.combined.scl(PPM));
@@ -103,17 +131,81 @@ public class Application extends ApplicationAdapter {
     }
 
     public void inputUpdate(float delta) {
-        int horizontalForce = 0;
-
         if (Gdx.input.isKeyPressed((Input.Keys.LEFT))) {
             horizontalForce -= 1;
+
         }
         if (Gdx.input.isKeyPressed((Input.Keys.RIGHT))) {
             horizontalForce += 1;
         }
-        if (Gdx.input.isKeyJustPressed((Input.Keys.SPACE))) {
-            golfBall.applyForceToCenter(0, 500, false);
+//        if (Gdx.input.isKeyJustPressed((Input.Keys.SPACE))) {
+//            golfBall.applyForceToCenter(0, 500, false);
+//        }
+
+
+//        if (Gdx.input.isKeyPressed(Input.Keys.LEFT) && horizontalForce == 0) {
+//            if (redBallX - ballX > -25) {
+//                redBallX -= 2;
+//                if (redBallX - ballX - 5 > 0) {
+//                    redBallY += 2;
+//                } else if (redBallX - ballX < 0) {
+//                    redBallY -= 2;
+//                }
+//                up = (redBallY - ballY - 5) / 25;
+//                right = (redBallX - ballX - 5) / 30;
+//            }
+//        } else if (Gdx.input.isKeyPressed(Input.Keys.RIGHT) && horizontalForce == 0) {
+//            if (redBallX - ballX < 35) {
+//                redBallX += 2;
+//                if (redBallX - ballX - 10 > 0) {
+//                    redBallY -= 2;
+//                } else if (redBallX - ballX - 5 < 0) {
+//                    redBallY += 2;
+//                }
+//                up = (redBallY - ballY - 5) / 25;
+//                right = (redBallX - ballX - 5) / 30;
+//            }
+//        } else
+        if (Gdx.input.isKeyPressed(Input.Keys.SPACE) && horizontalForce <= .1) {
+            horizontalForce = 0;
+//            ballAngle = 0;
+            if (powerUp) {
+                if (powerBallSize >= (golfBallSize * 8)) {
+                    powerUp = false;
+                } else {
+                    powerBallSize *= 1.15f;
+                }
+            } else {
+                if (powerBallSize <= (golfBallSize + 1)) {
+                    powerBallSize = golfBallSize;
+                    powerUp = true;
+                } else {
+                    powerBallSize *= 0.85f;
+                }
+            }
         }
+
+//        if (horizontalForce > 0) {
+//            ballX += (movingPower * right);
+//            ballY += (movingPower * up);
+//            redBallX = -10;
+//            redBallY = -10;
+//            movingPower = (float) (movingPower * 0.96);
+//            if (movingPower <= .5 && ballY <= 250.01) {
+//                movingPower = 0;
+//                velocityY = 0;
+//                ballY = 250;
+//                redBallX = ballX + 35;
+//                redBallY = ballY + 5;
+//                up = 0;
+//                right = 1;
+//            }
+//        }
+//        if (power > 0) {
+//            movingPower = (float) (power * 1.6);
+//            power = 0;
+//        }
+
 
         golfBall.setLinearVelocity(horizontalForce * 5, golfBall.getLinearVelocity().y);
     }
@@ -157,7 +249,7 @@ public class Application extends ApplicationAdapter {
         return golfBallBody;
     }
 
-    public Body createBox(int x, int y, int width, int height, boolean isStatic) {
+    public Body createBox(float x, float y, float width, float height, boolean isStatic) {
         Body body;
         BodyDef def = new BodyDef();
         if (isStatic) def.type = BodyDef.BodyType.StaticBody;
@@ -167,7 +259,7 @@ public class Application extends ApplicationAdapter {
         body = world.createBody(def);
 
         PolygonShape shape = new PolygonShape(); // TODO change to circle
-        shape.setAsBox(32 / 2 / PPM, 32 / 2 / PPM);
+        shape.setAsBox((float) 32 / 2 / PPM, (float) 32 / 2 / PPM);
 
         body.createFixture(shape, 1.0f);
         shape.dispose();
