e3c9ebcc86dc7bc7baed09faacdd55f767b78a41
jck1044 <jck1044@wildcats.unh.edu>
2024-03-28 13:59:43 -0400

    add more power to each shot

--- core/src/com/mygdx/infinitegolf/scene/Hole.java
+++ core/src/com/mygdx/infinitegolf/scene/Hole.java
@@ -280,8 +280,8 @@ public class Hole extends Scene {
     public void updateGolfBallPosition(float delta) {
         if (!Gdx.input.isKeyPressed(Input.Keys.SPACE)) {
             if (powerBallController.getSize() > golfBallSize) {
-                float horizontalForce = (90 - arrowController.getAngle()) * powerBallController.getSize() / 3;
-                float verticalForce = getVerticalForce(arrowController.getAngle()) * powerBallController.getSize() / 5;
+                float horizontalForce = (90 - arrowController.getAngle()) * powerBallController.getSize() / 2.5f;
+                float verticalForce = getVerticalForce(arrowController.getAngle()) * powerBallController.getSize() / 2.5f;
                 golfBallBody.applyForceToCenter(horizontalForce, verticalForce, false);
                 powerBallController.setSize(golfBallSize);
                 holeShotCounter++;
